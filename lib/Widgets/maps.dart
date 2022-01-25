f8fa4344a377e980a98a13ecede032144b794abf
Shaon Baidya <kenkaneki0625@gmail.com>
2022-01-25 15:52:53 +0530

    google maps integration

---
+++ lib/Widgets/maps.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+import '../Widgets/sidebar.dart';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapsScreen extends StatefulWidget {
+  @override
+  State<MapsScreen> createState() => MapsScreenState();
+}
+
+class MapsScreenState extends State<MapsScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final _kGooglePlexMarker = Marker(
+      markerId: MarkerId('_kGooglePlex'),
+      infoWindow: InfoWindow(title: 'Google Plex'),
+      icon: BitmapDescriptor.defaultMarker,
+      position: LatLng(37.42796133580664, -122.085749655962));
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  static final _kLakeMarker = Marker(
+      markerId: MarkerId('_kLake'),
+      infoWindow: InfoWindow(title: 'Lake'),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+      position: LatLng(37.43296265331129, -122.08832357078792));
+
+  static final Polyline _kPolyline = Polyline(
+    polylineId: PolylineId('_kPolyline'),
+    points: [
+      LatLng(37.42796133580664, -122.085749655962),
+      LatLng(37.43296265331129, -122.08832357078792)
+    ],
+    width: 5,
+  );
+
+  static final Polygon _kPolygon = Polygon(
+    polygonId: PolygonId('_kPolygon'),
+    points: [
+      LatLng(37.42796133580664, -122.085749655962),
+      LatLng(37.43296265331129, -122.08832357078792),
+      LatLng(37.418, -122.092),
+      LatLng(37.435, -122.092)
+    ],
+    strokeWidth: 2,
+    strokeColor: Colors.redAccent,
+    fillColor: Colors.transparent
+    );
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      drawer: NavigationDrawerWidget(),
+      appBar: AppBar(title: Text('Google Maps integration')),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        markers: {_kGooglePlexMarker, _kLakeMarker},
+        polylines: {_kPolyline},
+        polygons: {_kPolygon},
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
